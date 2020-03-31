1174b14b9a039300256c7879950c0cf3edbfef61
subbareddy <subbufantastic1234@gmail.com>
2020-03-31 14:40:07 +0530

    new project

---
+++ src/util/java/Programs.java
@@ -0,0 +1,20 @@
+package util.java;
+
+public class Programs {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+		for( x=1;x<=5;x++){
+			for(y=5;y>=x;y--){
+			System.out.print("* ");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
+
+
+
